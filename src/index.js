4d0c008ae47c105ab5d1ebb3314373ad5f2cc1d8
Adityuhh11 <saipremaditya@gmail.com>
2025-07-15 22:31:25 +0200

    Utils code added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
+
+
 dotenv.config({
  path: "./env"
 })
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,9 @@
+const asynchandler = (requiredFunction) => async (req,res,next)=>{
+        try {
+             await requiredFunction(req,res,next);
+        } catch (error) {
+            console.log("Error:", error);
+            next(error);
+        }
+       
+    }
