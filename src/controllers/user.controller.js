7620548a5c8c697580a6e0c0a05ac35720e30b04
Adityuhh11 <saipremaditya@gmail.com>
2025-07-27 13:44:39 +0200

    Refactor user registration process: add validation, cloudinary upload, and fix typos in user model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,79 @@
 import {asynchandler} from "../utils/asynchandler.js"
-
-
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.mode.js"
+import {uploadOnCloudinary} from "../utils/clouddinary.js"
+import {ApiResponse} from "../utils/ApiRespons.js"
+// get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
 const registerUser =  asynchandler(async(req,res)=>{
-    res.status(200).json({
-        messge:"ma chudao bc "
+   const {fullName , email ,Username, password} = req.body
+
+//    if ([fullName , email ,Username, password].some((field)=>field?.trim()==="")){
+//     throw new ApiError(400,"All Fields are requrired")
+//    }
+    if (fullName === "") {
+        throw new ApiError(400,"All Fields are requrired")
+    } else if  (email ==="") {
+        throw new ApiError(400,"All Fields are requrired")
+    } else if(Username) {
+        throw new ApiError(400,"All Fields are requrired")
+    }else if(password) {
+        throw new ApiError(400,"All Fields are requrired")
+    }
+
+   const existingUser = await User.findOne({
+    $or:[{ Username }, {email}]
+   })
+
+
+   if (existingUser){
+    throw new ApiError(409,"User with email or username aldready exists")
+   }
+
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+   const coverimageLocalPath = req.fields?.coverimage[0]?.path;
+
+
+   if (!avatarLocalPath) {
+     throw new ApiError(400, "Avatar is Required")
+   }
+
+    const avatar =await uploadOnCloudinary(avatarLocalPath)
+    const coverimage =await uploadOnCloudinary(coverimageLocalPath)
+
+    if (!avatar) {
+        throw new ApiError(400, "Avatar is Required")
+    }
+
+    const user = User.create({
+        fullName, 
+        avatar: avatar.url,
+        coverimage : coverimage.url || "",
+        email,
+        Username: Username.LowerCase(),
+        password
     })
+
+    const createdUser = await User.findById(user.__id).select(
+
+        "-password -refreshtoken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500 , "Something went wrong while regesitering the user")
+    } 
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"userRegisterd Successfully")
+    )
 })
+
 
 export {registerUser}
--- src/models/user.mode.js
+++ src/models/user.mode.js
@@ -7,12 +7,12 @@ const UserSchema =  new mongoose.Schema (
     {
     Id:{
         type:String,
-        requried: true,
+        required: true,
     },
     username:{
         type: String,
         unique: true,
-        requried:true,
+        required:true,
         lowercase:true,
         trim:true,
         index:true,
@@ -20,19 +20,19 @@ const UserSchema =  new mongoose.Schema (
     email:{
         type: String,
         unique: true,
-        requried:true,
+        required:true,
         lowercase:true,
         trim:true,
     },
     fullName:{
         type: String,
-        requried:true,
+        required:true,
         trim:true,
         index:true,
     },
     avatar:{
         type: String,
-        requried:true,
+        required:true,
     },
     coverimage:
     {type: String,
@@ -44,7 +44,7 @@ const UserSchema =  new mongoose.Schema (
     },
     password:{
         type: String,
-        requried:[true , 'password is required']
+        required:[true , 'password is required']
     },
     refreshToken:{
         type: String,
@@ -53,41 +53,46 @@ const UserSchema =  new mongoose.Schema (
 },{ timestamps: true })
 
 
-UserSchema.pre("save",async function (next){
+UserSchema.pre("save", async function (next)
+{
     if(!this.isModified("password")){return next()};
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
+
+
 })
 UserSchema.methods.isPasswordCorrect = async function(password){
    return await bcrypt.compare(password,this.password) 
 }
 
 UserSchema.methods.generateAccessToken = function(){
-    jwt.sign({
+    return jwt.sign({
         _id :this._id,
         email:this.email,
         username :this.username,
         fullName:this.fullName
 
-    })
-    process.env.ACCESS_TOKEN_SECRET,
-    {
-        expriesIn:process.env.ACCESS_TOKEN_EXPIRY
-    }
+    },
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+}
+);
 }
 UserSchema.methods.generateRefreshTokens = function(){
-      jwt.sign({
+      return jwt.sign({
         _id :this._id,
-    })
-    process.env.REFRESH_TOKEN_SECRET,
-    {
-        expriesIn:process.env.REFRESH_TOKEN_EXPIRY
-    }
+    },
+process.env.REFRESH_TOKEN_SECRET,
+{
+    expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+}
+)
 }
 
 
-export const User = MongooseError.model("User", UserSchema)
+export const User = mongoose.model("User", UserSchema)
 
 
 
