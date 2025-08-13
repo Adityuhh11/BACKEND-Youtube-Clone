a5a69bde2ef47792acb48198104f0730809bae87
Adityuhh11 <saipremaditya@gmail.com>
2025-08-13 18:06:14 +0200

    add User Routes for Updation and important endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,20 @@
 import {Router} from "express";
-import { logoutUser, registerUser, loginUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeUserPassword,
+    getCurrentUser,
+    UpdateAccountDetails,
+    updateUsercoverImage,
+    updateUserAvatar
+    } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 const router =  Router()
 import { VerifyJWT } from "../middlewares/auth.middleware.js";
+
+
 
 
 router.route("/register").post(
@@ -12,7 +24,7 @@ router.route("/register").post(
             maxCount:1
         },
         {
-            name:"coverimage",
+            name:"coverImage",
             maxCount:1
         }
     ]),
@@ -24,6 +36,24 @@ router.route("/login").post(loginUser)
 //secure route
 router.route("/logout").post(VerifyJWT, logoutUser)
 router.route("/refresToken").post(refreshAccessToken)
+router.route("/changePassword").post(VerifyJWT,changeUserPassword)
+router.route("/user").get(VerifyJWT,getCurrentUser)
+router.route("/updateDetails").post(VerifyJWT, UpdateAccountDetails)
+router.route("/update/Avatar").post(VerifyJWT,upload.fields([
+    {
+        name:"avatar",
+        maxCount:1
+    } 
+]), 
+    updateUserAvatar)
+router.route("/update/coverImage").post(VerifyJWT,
+    upload.fields([
+    {
+        name:"coverImage",
+        maxCount:1
+    } 
+]),
+     updateUsercoverImage)
 
 
 export default router
