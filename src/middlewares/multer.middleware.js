00d9412066dd90ce944807403bce412fc3d83a37
Adityuhh11 <saipremaditya@gmail.com>
2025-08-15 19:42:36 +0200

    accept videos on multer

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -10,4 +10,23 @@ const storage = multer.diskStorage({
   }
 })
 
+const VIDEO_MIMETYPES = ["video/mp4", "video/x-flv", "video/mkv", "video/webm"];
+const IMAGE_MIMETYPES = ["image/jpeg", "image/png", "image/gif"];
+
 export const upload = multer({ storage: storage })
+
+export const videoUpload = multer(
+  {
+    storage:storage,
+   limits:{fileSize:20*1024*1024},
+   fileFilter:{
+    function(req,res,cb){
+      if (!file.mimetype.startsWith("video/") && !file.mimetype.startsWith("image/")) {
+    return cb(new Error("File type not allowed!"), false);
+    } 
+    cb(null,true);
+    }
+   }
+
+  }
+)
