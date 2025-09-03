a3a0402593fcc8f723001dd39ae38355e1ea0458
Adityuhh11 <saipremaditya@gmail.com>
2025-09-03 23:42:13 +0200

    feat: add axios dependency and implement downloadFile utility for file downloads

---
+++ src/utils/downloadCloudinary.js
@@ -0,0 +1,27 @@
+// src/utils/downloader.js
+
+import axios from "axios";
+import fs from "fs";
+import path from "path"; // Import the 'path' module
+
+const downloadFile = async(url, destinationPath) => {
+    try {
+        const response = await axios.get(url, { responseType: "stream" });
+        const writer = fs.createWriteStream(destinationPath);
+        
+        response.data.pipe(writer);
+        
+        return new Promise((resolve, reject) => {
+            writer.on("finish", () => {
+                console.log(`Successfully downloaded file to ${destinationPath}`);
+                resolve();
+            });
+            writer.on("error", reject); 
+        });
+    } catch (error) {
+
+        throw error;
+    }
+};
+
+export default downloadFile;
